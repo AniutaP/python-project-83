5051c2be8ca98923ca11e230921ae7f507243d8e
AniutaP <musenok.ko@mail.ru>
2023-10-04 03:32:42 +0300

    add tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,11 @@
 from page_analyzer.app import app
 
 
-def test_index():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.data == b'Welcome to My project!'
+def test_home():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+
+
+def test_urls_conteiner():
+    response = app.test_client().get('/urls')
+    assert response.status_code == 200
