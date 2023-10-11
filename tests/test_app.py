2c5d731dd352c84755c3e37c027b9f7cebd5bf28
AniutaP <musenok.ko@mail.ru>
2023-10-12 02:36:10 +0300

    fix test

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,9 +3,7 @@ from page_analyzer.app import app
 
 def test_home():
     response = app.test_client().get('/')
+    html = response.data.decode()
     assert response.status_code == 200
-
-
-def test_urls_table():
-    response = app.test_client().get('/urls')
-    assert response.status_code == 200
+    assert '<a class="navbar-brand" href="/">Анализатор страниц</a>' in html
+    assert '<a class="nav-link " href="/urls">Сайты</a>' in html
