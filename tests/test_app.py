6b50f47075bdbd3b30dc569e3f508b335a58a844
AniutaP <musenok.ko@mail.ru>
2023-10-15 18:12:49 +0300

    update test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
 from page_analyzer.app import app
+from page_analyzer.url_check import validate
+from page_analyzer.get_data import get_url_by_name
+import string
+import random
 
 
 def test_home():
@@ -7,3 +11,15 @@ def test_home():
     assert response.status_code == 200
     assert '<a class="navbar-brand" href="/">Анализатор страниц</a>' in html
     assert '<a class="nav-link" href="/urls">Сайты</a>' in html
+
+
+def test_validate():
+    url = 'https://check.su/contact'
+    assert validate(url) == {'url': 'https://check.su', 'error': None}
+
+    url = random.choices(string.ascii_letters + string.digits, k=256)
+    assert validate(url) == {'url': url, 'error': 'Слишком длинный URL'}
+
+    url = 'bckajsbc'
+    assert validate(url) == {'url': 'bckajsbc', 'error': 'Некорректный URL'}
+
