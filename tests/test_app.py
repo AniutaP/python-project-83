983a965263d4f782b93cefcda32d5e63d580949d
AniutaP <musenok.ko@mail.ru>
2023-11-03 14:48:51 +0300

    update

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,11 +2,19 @@ from page_analyzer.app import app
 from page_analyzer.url_check import validate
 import string
 import random
+import pytest
 
 
-def test_home():
+@pytest.fixture
+def client():
     app.config['TESTING'] = True
-    response = app.test_client().get('/')
+    client = app.test_client()
+    yield client
+
+
+def test_home(client):
+    app.config['TESTING'] = True
+    response = client.get('/')
     html = response.data.decode()
     assert response.status_code == 200
     assert '<a class="navbar-brand" href="/">Анализатор страниц</a>' in html
