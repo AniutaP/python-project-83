11093ede175ec80934446b5abe6f4d020bba9110
AniutaP <musenok.ko@mail.ru>
2023-11-20 17:39:23 +0300

    update

--- page_analyzer/get_conn.py
+++ page_analyzer/get_conn.py
@@ -6,25 +6,22 @@ from contextlib import contextmanager
 postgresql_pool = None
 
 
-def init_db_pool():
+@contextmanager
+def get_connection():
     global postgresql_pool
+    connection = None
     DATABASE_URL = os.getenv('DATABASE_URL')
     APP_ENV = os.getenv('APP_ENV')
     if APP_ENV != "tests":
         postgresql_pool = pool.SimpleConnectionPool(minconn=1,
                                                     maxconn=10,
                                                     dsn=DATABASE_URL)
-
-
-@contextmanager
-def get_connection():
-    connection = None
-    try:
-        connection = postgresql_pool.getconn()
-        yield connection
-        connection.commit()
-    except Exception as error:
-        connection.rollback()
-        raise error
-    finally:
-        postgresql_pool.putconn(connection)
+        try:
+            connection = postgresql_pool.getconn()
+            yield connection
+            connection.commit()
+        except Exception as error:
+            connection.rollback()
+            raise error
+        finally:
+            postgresql_pool.putconn(connection)
